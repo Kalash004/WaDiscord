dfd776c5b96dff86c8f489e1e4880d7a547be126
Kalash004 <kalashnikov004@gmail.com>
2024-01-20 21:39:03 +0100

    fixed too many connections err

--- dbconn.js
+++ dbconn.js
@@ -1,8 +1,9 @@
 import mysql  from 'mysql2/promise';
 import config from "./dbconfig.js";
 
+const connection = await mysql.createConnection(config.db)
+
 export async function query(sql, params) {
-    const connection = await mysql.createConnection(config.db);
     const [rows,] = await connection.execute(sql, params);
     return rows;
 }
